cd3a87d4322d7a06c4631156795eb9b2bbe3d17e
ZacRayTho <zachary.thomas@student.awesomeinc.org>
2023-02-14 22:06:33 +0000

    randomuser API working with nationality and gender filter

--- script.js
+++ script.js
@@ -15,92 +15,87 @@ let age = document.getElementById("age");
 let nationTest = document.getElementById("nation")
 
 
-btn.addEventListener( "click", ()=> {
-	for (let x of radioButtons) {
-	  if (x.checked) {
-        genderPref = x.value;
-       break;
-    } 
-  }
+// btn.addEventListener( "click", ()=> {
+// 	for (let x of radioButtons) {
+// 	  if (x.checked) {
+//         genderPref = x.value;
+//        break;
+//     } 
+//   }
   
-   apiCall(); 
+//    apiCall(); 
+// })
+
+
+// function apiCall() {
+//   let options = {
+//     baseURL: BASE_URL,
+//     params: {
+//       gender: genderPref,
+//       nat: nationTest.value,
+//     }
+//   }
+//   //axios.get('/user?ID=12345') same as below but not able to be modified
+//   axios.get('/api/', options)
+//     .then(function (response) {
+//       console.log(response);
+//       state.user = response.data.results[0];
+//       makePage();
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     })
+//     .then(function () {
+//     // always executed
+//     });
+// }
+
+// let state = {
+//    user: null,
+// }
+
+// function makePage() {
+// img.src = state.user.picture.large
+// nameboo.innerHTML = state.user.name.first + " " + state.user.name.last;
+// city.innerHTML = state.user.location.city + " " + state.user.location.postcode;
+// age.innerHTML = state.user.dob.age;
+// }
+
+
+
+
+
+
+
+let url = 'https://swapi.dev/api/people/';
+let jokes = [];
+let x = url;
+btn.addEventListener( "click", ()=> {
+  apiCCall()
 })
 
+function apiCCall() {
+  axios.get(url)
+  .then(function (response) {
+    jokes = response.data;
+    url = jokes.next;
+    console.log("HERE", jokes);
+    createListItems(jokes.results);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+}
 
-function apiCall() {
-  let options = {
-    baseURL: BASE_URL,
-    params: {
-      gender: genderPref,
-      nat: nationTest.value,
+
+function createListItems(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let listItem = document.createElement('li');
+        listItem.innerText = `${jokes.results[i].name}`;
+        document.getElementById('age').appendChild(listItem);
     }
-  }
-  //axios.get('/user?ID=12345') same as below but not able to be modified
-  axios.get('/api/', options)
-    .then(function (response) {
-      console.log(response);
-      state.user = response.data.results[0];
-      makePage();
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .then(function () {
-    // always executed
-    });
 }
-
-let state = {
-   user: null,
-}
-
-function makePage() {
-img.src = state.user.picture.large
-nameboo.innerHTML = state.user.name.first + " " + state.user.name.last;
-city.innerHTML = state.user.location.city + " " + state.user.location.postcode;
-age.innerHTML = state.user.dob.age;
-}
-
-
-
-
-
-
-
-// let url = 'https://swapi.dev/api/people/';
-// let jokes = [];
-// let next = "h"
-// let x = url;
-// console.log(axios.get(url));
-// for (let i = 0; i < 10; i++) {
-  
-//   //don't work right,does the get 10 times then does the rest
-// axios.get(x)
-//   .then(function (response) {
-//     // handle success
-//     // console.log(response);
-//     // console.log('JOKES', response.data);
-//     jokes = response.data;
-//     next = jokes.next;
-//     console.log("HERE", jokes);
-//     createListItems(jokes.results);
-//     x = next;
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-// }
- 
-
-
-// function createListItems(arr) {
-//     for (let i = 0; i < arr.length; i++) {
-//         let listItem = document.createElement('li');
-//         listItem.innerText = `${jokes.results[i].name}`;
-//         document.getElementById('myList').appendChild(listItem);
-//     }
-// }
   
 
 // let myPromise = new Promise((resolve, reject) => {
